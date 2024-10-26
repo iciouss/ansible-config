b5fa0441ce53d846fe3dffbb01a1747abb4b9833
Miguel Moniente <25775738+iciouss@users.noreply.github.com>
2024-10-27 00:59:28 +0200

    fix error in bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,3 @@
 rm -rf /etc/ansible/*
 git clone https://github.com/iciouss/ansible-config.git /etc/ansible
-ansible-playbook /etc/ansible/playbooks/initial-setup.yaml
+ansible-playbook /etc/ansible/playbooks/initial-config.yaml
