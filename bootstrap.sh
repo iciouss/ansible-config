11f9e7bed97da94321502c8e1d91cd4350f6f9ad
Miguel Moniente <25775738+iciouss@users.noreply.github.com>
2024-10-28 22:52:59 +0100

    fix error in zsh config and add option to install k3s from bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,6 @@
 rm -rf /etc/ansible/*
 git clone https://github.com/iciouss/ansible-config.git /etc/ansible
 ansible-playbook /etc/ansible/playbooks/initial-config.yaml
+if [ $INSTALL_MODE = "k3s" ]; then
+    ansible-playbook /etc/ansible/playbooks/k3s-config.yaml
+fi
